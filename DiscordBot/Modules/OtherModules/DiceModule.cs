724a97f48307b3d9d9d3952653c9dc3834c9f576
Meow Nea <aquamusic0623@gmail.com>
2025-02-20 02:49:22 +0900

    Diceコマンドを作った

---
+++ DiscordBot/Modules/OtherModules/DiceModule.cs
@@ -0,0 +1,17 @@
+﻿namespace DiscordBot.Modules.OtherModules;
+
+public class DiceModule : InteractionModuleBase<SocketInteractionContext>
+{
+    // <summary>
+    // サイコロを振るコマンド
+    // </summary>
+    [SlashCommand("dice", "サイコロを振ります。")]
+    public async Task DiceCommandAsync([Summary(description: "振るサイコロの面数を入力してください。")] int DiceNumber = 0)
+    {
+        if (DiceNumber <= 0) DiceNumber = 6;
+        var random = new Random();
+
+        var result = random.Next(1, DiceNumber + 1);
+        await RespondAsync(result.ToString());
+    }
+}
