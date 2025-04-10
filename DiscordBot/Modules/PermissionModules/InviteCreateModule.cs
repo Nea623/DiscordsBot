ae0c65cb732deacecee3ae5fa9f666540fc20284
Meow Nea <aquamusic0623@gmail.com>
2025-04-10 17:23:30 +0900

    InviteCreateModuleの変更

--- DiscordBot/Modules/PermissionModules/InviteCreateModule.cs
+++ DiscordBot/Modules/PermissionModules/InviteCreateModule.cs
@@ -1,21 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DiscordBot.Modules.PermissionModules;
 
-namespace DiscordBot.Modules.ServerModules;
 public class InviteCreateModule : InteractionModuleBase<SocketInteractionContext>
 {
     // <summary>
     // 無制限の招待リンクを作成して実行したチャンネルにピン留めするコマンド
     // </summary>
     [SlashCommand("invite", "無制限の招待リンクを作成して実行したチャンネルにピン留めします。")]
+    [Discord.Commands.RequireBotPermission(GuildPermission.CreateInstantInvite)]
+    [Discord.Commands.RequireUserPermission(GuildPermission.CreateInstantInvite)]
     public async Task InviteCreateCommandAsync()
     {
         var invite = await Context.Guild.SystemChannel.CreateInviteAsync(maxAge: null, maxUses: null);
-        var message = await Context.Channel.SendMessageAsync($"無制限の招待リンクを作成しました。\n" + 
-                                               $"{invite.Url}");
+        await RespondAsync("無制限の招待リンクを作成しました。\n" + 
+                          $"{invite.Url}");
+        var message = await GetOriginalResponseAsync();
         await message.PinAsync();
     }
 }
