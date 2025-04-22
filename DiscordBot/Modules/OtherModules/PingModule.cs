10caae7348c6a3ba2e3c3f816297c1a5ccb275fd
Meow Nea <aquamusic0623@gmail.com>
2025-04-23 00:19:18 +0900

    Pingのlatencyのズレを修正した。

--- DiscordBot/Modules/OtherModules/PingModule.cs
+++ DiscordBot/Modules/OtherModules/PingModule.cs
@@ -4,8 +4,8 @@ namespace DiscordBot.Modules.OtherModules;
 
 public class PingModule : InteractionModuleBase<SocketInteractionContext>
 {
-    /* <summary>
-    // 新pingコマンド(WebSocket PingとAPI Endpoint Pingを取得するもの)
+    // <summary>
+    // pingコマンド(WebSocket PingとAPI Endpoint Pingを取得するもの)
     // </summary>
     [IntegrationType(ApplicationIntegrationType.GuildInstall | ApplicationIntegrationType.UserInstall)]
     [SlashCommand("ping", "pingの計測を行います。")]
@@ -20,35 +20,9 @@ public class PingModule : InteractionModuleBase<SocketInteractionContext>
         await RespondAsync(embed: embedBuilder.Build(), ephemeral: true);
         var response = await GetOriginalResponseAsync();
 
-        var latency = (DateTime.UtcNow - response.CreatedAt).Milliseconds;
+        var latency = ((DateTime.Now + TimeSpan.FromSeconds(1)) - response.CreatedAt).Milliseconds;
         embedBuilder.Description += $"API Endpoint Ping: {latency}ms";
 
         await ModifyOriginalResponseAsync(x => x.Embed = embedBuilder.Build());
-    }
-    */
-
-    // <summary>
-    // 旧pingコマンド(ただ単に、アクセスできるかどうか、レスポンスタイムを取得しているだけのもの)
-    // </summary>
-    [SlashCommand("ping", "pingを計測行います。")]
-    public async Task PingCommandAsync([Summary(description: "URLを指定してください。")] string url = "discord.com")
-    {
-        using (var ping = new Ping())
-        {
-            var result = ping.Send(url);
-
-            if (result.Status == IPStatus.Success)
-            {
-                var embedBuilder = new EmbedBuilder()
-                    .WithTitle("Pong! :ping_pong:")
-                    .AddField("送信したURL", url, true)
-                    .AddField("かかった時間", result.RoundtripTime + "ms", true)
-                    .WithFooter($"実行者: {Context.User.GlobalName ?? Context.User.Username}", Context.User.GetDisplayAvatarUrl())
-                    .WithColor(0x8DCE3E);
-
-                await RespondAsync(embed: embedBuilder.Build());
-            }
-            else await RespondAsync("失敗: pingの送信に失敗しました。");
-        }
     }
 }
